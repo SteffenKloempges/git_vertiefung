1e475cdab1580a267ec848b348552ff8caa33711
SteffenKloempges <steffen.kloempges@gmail.com>
2023-02-23 09:46:39 +0100

    built onclick function verdoppeln in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("it works");
+
+const inputNumber = document.querySelector('form input[type="number"]');
+
+console.log(inputNumber)
+
+function verdoppeln() {
+    console.log("func works");
+    // let input = inputNumber.value * 2
+    // console.log(input);
+    console.log(inputNumber.value * 2)
+}
